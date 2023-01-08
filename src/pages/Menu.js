703099ddc6cb21bb1b45e25a93ad3e88ccedcd61
raizadattn <abhimanyu.raizada@tothenew.com>
2023-01-08 18:13:12 +0530

    Grid done

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -91,19 +91,19 @@ function Menu() {
       </div>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid item xs={6} md={4}>
+          <Grid item  lg={6}>
             {/* <Item>xs=8</Item> */}
             <ItemCard />
           </Grid>
-          <Grid item xs={6} md={4}>
+          <Grid item  lg={6}>
             {/* <Item>xs=4</Item> */}
             <ItemCard />
           </Grid>
-          <Grid item xs={6} md={4}>
+          <Grid item  lg={6}>
             {/* <Item>xs=4</Item> */}
             <ItemCard />
           </Grid>
-          <Grid item xs={6} md={4}>
+          <Grid item  lg={6}>
             {/* <Item>xs=8</Item> */}
             <ItemCard />
           </Grid>
