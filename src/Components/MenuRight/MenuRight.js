5790f8affd10e81d42e22c47239570a1aa5da57c
prashantkaushik97 <kaushik.prashant00@gmail.com>
2023-01-07 21:10:24 -0500

    menu changes

--- src/Components/MenuRight/MenuRight.js
+++ src/Components/MenuRight/MenuRight.js
@@ -1,8 +1,15 @@
-import { Box, Button, Card, CardContent, CardMedia, Grid, Typography } from '@material-ui/core'
+import { Box, Button, Card, CardContent, CardMedia, Grid, Input, Typography, } from '@material-ui/core'
 import React from 'react'
+import AddIcon from '@mui/icons-material/Add';
+
 import Item from '../../common-components/coursel/Item'
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Badge from "@material-ui/core/Badge";
+import RemoveIcon from '@mui/icons-material/Remove';
 import './Style.css'
 function MenuRight() {
+  const [itemCount, setItemCount] = React.useState(1);
+
   return (
     <div>
       <div className='submenu__wrapper'>
@@ -22,41 +29,65 @@ function MenuRight() {
           <div>
             <h3>Paneer Biryani</h3>
             <p>
-            A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
+              A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
             </p>
-            <Button>uk</Button>
-          </div>
-          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png'/>
+            <Badge color="secondary" badgeContent={itemCount}>
+            </Badge>
+            <ButtonGroup>
+              <Button
+                onClick={() => {
+                  setItemCount(Math.max(itemCount - 1, 0));
+                }}
+              >
+                <RemoveIcon fontSize="small" />
+
+              </Button>
+              <Input
+                className='input-quanity'
+                value={itemCount}
+              />
+              <Button
+                onClick={() => {
+                  setItemCount(itemCount + 1);
+                }}
+              >
+                <AddIcon fontSize="small" />
+              </Button>
+
+            </ButtonGroup>          </div>
+          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png' />
         </div>
         <div className="menu__item_wrapper">
           <div>
             <h3>Paneer Biryani</h3>
             <p>
-            A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
+              A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
             </p>
             <Button>uk</Button>
           </div>
-          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png'/>
+          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png' />
         </div>
         <div className="menu__item_wrapper">
           <div>
             <h3>Paneer Biryani</h3>
             <p>
-            A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
+              A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
             </p>
             <Button>uk</Button>
           </div>
-          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png'/>
+          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png' />
         </div>
         <div className="menu__item_wrapper">
           <div>
             <h3>Paneer Biryani</h3>
             <p>
-            A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
+              A spiced mix of Paneer and rice, traditionally cooked over an open fire in a leather pot
             </p>
-            <Button>uk</Button>
+            <div>
+
+            </div>
           </div>
-          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png'/>
+          <img src='https://thebellyrulesthemind.net/wp-content/uploads/2020/02/instant-pot-paneer-biryani-720x540.png' />
         </div>
       </div>
     </div>
--- src/common-components/category-card/CategoryCard.js
+++ src/common-components/category-card/CategoryCard.js
@@ -20,14 +20,14 @@ export default function CategoryCard() {
 `;
 
   return (
-<div class="card__wrapper">
- <div class="card">
-    <figure class="card__thumbnail">
-      <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60"/>
-      <span class="card__title">Category</span>
-    </figure>
-  </div>
+    <div class="card__wrapper">
+      <div class="card">
+        <figure class="card__thumbnail">
+          <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60" />
+          <span class="card__title">Category</span>
+        </figure>
+      </div>
     </div>
-   
+
   );
 }
